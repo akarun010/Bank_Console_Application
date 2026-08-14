15213f53f3dbf5a925189daf2bd7d4fed3c06bf6
Arun <akarunff0@gmail.com>
2026-08-14 15:32:20 +0530

    Configured Database Connection

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -1,5 +1,24 @@
 package util;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+	private static final String url = "jdbc:mysql://localhost:3306/student_management";
+	private static final String user = "root";
+	private static final String password = "root";
+	private Connection connection;
+	
+	public DBConnection() {
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Connection successful!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public Connection getConnection() {
+		return connection;
+	}
 }

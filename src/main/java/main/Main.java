e1a6b42e87bc2d8ec3c082fafe22de78113c150b
Arun <akarunff0@gmail.com>
2026-08-15 14:15:53 +0530

    Main Class Written

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
+import service.StudentService;
+
 public class Main {
 
 	public static void main(String[] args) {
+		StudentService studService = new StudentService();
+		studService.start();
 	}
-
 }
